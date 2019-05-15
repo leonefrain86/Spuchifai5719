eb737c980526d09d3448c89128bba8c82e7cd838
efrain <leonefrain81@gmail.com>
2019-05-15 17:40:34 -0300

    Alcualizado commit

---
+++ Scripts/01 SP.sql
@@ -0,0 +1,47 @@
+create procedure altaBanda (unidBanda SMALLINT,
+                            unnombre VARCHAR(45),
+                            unanioFundacion SMALLINT)
+BEGIN
+    INSERT INTO Banda (idBanda, nombre, anioFundacion)
+                VALUES (unidBanda, unnombre, unaniofundacion)
+END
+
+create procedure altaAlbum (unidAlbum TINYINT,
+                            unidBanda SMALLINT,
+                            unnombre VARCHAR(45),
+                            unfechaLanzamiento DATE)
+BEGIN
+    INSERT INTO Album (idAlbum, idBanda, nombre, fechaLanzamiento)
+                VALUES (unidAlbum, unidBanda, unnombre, unfechaLanzamiento)
+END
+
+
+create procedure altaCancion (unidcancion INT,
+                            unidAlbum TINYINT,
+                            unnumeroOrden TINYINT,
+                            unnombre VARCHAR(45))
+BEGIN
+    INSERT INTO Cancion (idCancion, idAlbum, numeroOrden, nombre)
+                VALUES (unidCancion, unidAlbum, unnumeroOrden, unnombre)
+END
+
+
+create procedure altaReproduccion (unidReproduccion INT,
+                            unidCliente INT,
+                            unidCancion INT,
+                            unfechaHora DATETIME)
+BEGIN
+    INSERT INTO Reproduccion (idreproduccion, idCliente, idCancion, fechaHora)
+                VALUES (unidReproduccion, unidCliente, unidCancion, unfechaHora)
+END
+
+
+create procedure altaCliente (unidCliente INT,                            
+                            unnombre VARCHAR(45),
+                            unapellido VARCHAR(45),
+                            unmail VARCHAR(45),
+                            uncontrasenia VARCHAR(45))
+BEGIN
+    INSERT INTO Cliente (idCliente, nombre, apellido, mail, contrasenia)
+                VALUES (unidCliente, unnombre, unapellido, unmail, SHA(uncontrasenia))
+END
---
+++ Scripts/02 SF.sql
@@ -0,0 +1,27 @@
+DELIMITER//
+CREATE FUNCTION cantidadDeReproducciones( unIdCancion INTEGER )
+RETURNS INT
+BEGIN 
+        DECLARE TOTAL INT;
+        SELECT COUNT(idReproccion)
+        INTO TOTAL;
+        FROM Reproduccion
+        where idCancion = unIdCancion;
+
+        RETURN TOTAL;
+END//
+
+DELIMITER //
+CREATE FUNCTION cantidadDeReproduccionesPorBanda ( unIdBanda SMALLINT)
+RETURNS INT
+BEGIN 
+        DECLARE TOTAL;
+        SELECT SUM(cantidadDeReproducciones)
+        INTO TOTAL
+        FROM Album A 
+        INNER JOIN Cancion C
+        ON A.idAlbum = C.idAlbum
+        where A.idBanda = unIdBanda;
+
+        RETURN TOTAL;
+END//
