d599146aab3708e566afc96d397b3770ab93b9ac
Jean Paul <jeanpaulrey2@gmail.com>
2019-11-06 17:42:25 -0300

    Actualizacion
    iniciacion del menu de Adiministrador

---
+++ src/C Sharp/ORM/Spuchifai5719/AdministradorConsola/Menu/CrearBanda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MenuesConsola;
+using Spuchifai5719;
+using Spuchifai5719.ADO;
+
+namespace AdministradorConsola.Menu
+{
+    public class CrearBanda : MenuComponente
+    {
+        public IADO ADO { get; set; }
+        public Banda Banda { get; set; }
+        public override void mostrar()
+        {
+            //TODO LEERINFO DE LABANDA
+            //INSTANCIAR BANDA CON LA INFO QUE LEI
+            base.mostrar();
+            ADO.altaBanda(Banda);
+
+
+        }
+    }
+}
