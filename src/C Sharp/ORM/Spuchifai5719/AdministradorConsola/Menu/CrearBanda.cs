fc45a563560ee5ad960fe16c1b7294f2b4f6ff3b
leonefrain86 <leonefrain86cbo@gmail.com>
2019-11-06 18:13:27 -0300

    actualizado

--- src/C Sharp/ORM/Spuchifai5719/AdministradorConsola/Menu/CrearBanda.cs
+++ src/C Sharp/ORM/Spuchifai5719/AdministradorConsola/Menu/CrearBanda.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using MenuesConsola;
 using Spuchifai5719;
 using Spuchifai5719.ADO;
@@ -13,12 +11,17 @@ namespace AdministradorConsola.Menu
         public Banda Banda { get; set; }
         public override void mostrar()
         {
+            Console.WriteLine("Ingrese nombre de banda: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ingrese contraseña: ");
+            string pass = Console.ReadLine();
+
+            Banda = new Banda(name, pass);
             //TODO LEERINFO DE LABANDA
             //INSTANCIAR BANDA CON LA INFO QUE LEI
             base.mostrar();
             ADO.altaBanda(Banda);
-
-
         }
+
     }
 }
